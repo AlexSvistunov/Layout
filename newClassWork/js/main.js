e4c192d4eb369787bce09f408e850df8aebd8d67
AlexSvistunov <112976775+AlexSvistunov@users.noreply.github.com>
2023-06-06 08:37:54 +0300

    fix slider

--- newClassWork/js/main.js
+++ newClassWork/js/main.js
@@ -3,7 +3,8 @@ $(function(){
 
     $('.slider__inner').slick({
         dots: true,
-        arrows: false
+        prevArrow: '<button type="button" class="slick-prev"> <img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
 
     });
 
