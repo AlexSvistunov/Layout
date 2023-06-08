002943bb24729f0e1fd8492529e16b21b71f4931
AlexSvistunov <112976775+AlexSvistunov@users.noreply.github.com>
2023-06-08 14:52:13 +0300

    add 75% adaptive

--- homework-wave/js/main.js
+++ homework-wave/js/main.js
@@ -1,10 +1,19 @@
 $(function(){
-    var mixer = mixitup('.gallery__content');
+    
 
     $('.slider__inner-content').slick({
         dots: true,
         arrows: false
         
     });
+
+
+    $('.menu__btn').on('click', function(){
+        $('.header__top-inner').toggleClass('header__top-inner--active')
+
+    });
+
+
+    var mixer = mixitup('.gallery__content');
 
 });
