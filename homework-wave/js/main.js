03912fd57329b0ee4f683697dbff936a044f7745
AlexSvistunov <112976775+AlexSvistunov@users.noreply.github.com>
2023-06-10 10:11:38 +0300

    add smooth scroll and nav id

--- homework-wave/js/main.js
+++ homework-wave/js/main.js
@@ -1,4 +1,22 @@
 $(function(){
+
+    $(".menu a, .footer a").on("click",function(event){ 
+        //находим все ссылки в .menu
+        event.preventDefault();
+        //отменяем работу ссылок
+
+        var id = $(this).attr('href'),
+        //находит у каждой меню(attr - забирает) и забирает значение в href
+        // в  памяти хранит значение
+
+        top = $(id).offset().top;
+
+        //узнаем сколько нам нужно скроллить
+
+        $('body,html').animate({scrollTop:top}, 1000);
+        //1500 мл
+    });
+
     
 
     $('.slider__inner-content').slick({
@@ -8,7 +26,7 @@ $(function(){
     });
 
 
-    $('.menu__btn').on('click', function(){
+    $('.menu__btn, .menu a').on('click', function(){
         $('.header__top-inner').toggleClass('header__top-inner--active')
 
     });
